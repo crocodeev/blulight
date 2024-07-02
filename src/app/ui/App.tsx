ab765f72f878a1cc93fc237feee0bdf6c48f7f22
crocodeev <svin862003@mail.ru>
2024-07-02 14:56:59 +0300

    add gh-pages workflow

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { Button } from '../../shared/button';
+import { Selector } from '../../shared/selector';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <Selector />
+    </div>
+  );
+}
+
+export  { App };
